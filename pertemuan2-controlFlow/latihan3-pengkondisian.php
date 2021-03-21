389f6147ef3e3d15ac8af110c5b64553f8187ead
Riki Widiantoro <77282337+Riki-Toro@users.noreply.github.com>
2021-03-21 17:44:47 +0700

    Create latihan3-pengkondisian.php

---
+++ pertemuan2-controlFlow/latihan3-pengkondisian.php
@@ -0,0 +1,16 @@
+<?php 
+// PENGKONDISIAN / PERCABANGAN
+// if else, else if, ternary, switch
+
+// // if else, else if
+$x = 30;
+if ( $x < 20 ) {
+    echo "benar";
+} else if ($x == 20) {
+    echo "ini adalah 20";
+}  else {
+    echo "salah";
+}
+
+
+?>
