7ef3ae1b7a4812073ac5f525818d763f032e8d22
Riki Widiantoro <77282337+Riki-Toro@users.noreply.github.com>
2021-03-23 09:36:44 +0700

    Update function.php

--- pertemuan3-function/function.php
+++ pertemuan3-function/function.php
@@ -5,21 +5,21 @@
     // <!-- 1. build-in function >> function yang dimiliki php ada sangat banyak -->
     // <!-- Date/Time >> time(), date(), mktime(), strtotime() -->
 
-        // echo date("l, d-M-Y"); // untuk menampilkan tanggal hari ini
+        echo date("l, d-M-Y"); // untuk menampilkan tanggal hari ini
         
-        // echo time(); // UNIX Timestamp/EPOCH time >> detik yang sudah berlalu sejak 1 januari 1970
+        echo time(); // UNIX Timestamp/EPOCH time >> detik yang sudah berlalu sejak 1 januari 1970
 
         // cara mengetahui 100 hari dari sekarang
-        // echo date("l", time()+60*60*24*100);
+        echo date("l", time()+60*60*24*100);
         // 100 hari ke belakang
-        // echo date("l, d-M-Y", time()-60*60*24*100);
+        echo date("l, d-M-Y", time()-60*60*24*100);
 
 
         // membuat sendiri detik yang berlalu >> mktime()
         // mktime(0,0,0,0,0,0) >> jam, menit, detik, bulan, tanggal, tahun
-        // echo date("l", mktime(0,0,0,11,15,1998));
+        echo date("l", mktime(0,0,0,11,15,1998));
 
-        // echo date("l", strtotime("15 nov 1998"));
+        echo date("l", strtotime("15 nov 1998"));
 
     // string >> strlen(), strcmp(), explode(), htmlspecialchars()
     // utility >> var_dump(), isset(), empty(), die(), sleep()
