58d2d5ea01a8fde936cef6ba82ec51095a37fc6a
Riki Widiantoro <77282337+Riki-Toro@users.noreply.github.com>
2021-03-24 08:29:14 +0700

    Create latihan1.php

---
+++ pertemuan5-assosiatifArray/latihan1.php
@@ -0,0 +1,22 @@
+<!-- REVIEW MATERI ARRAY SEBELUMNYA -->
+<?php
+// array
+// membuat array
+// versi lama
+$hari = array("senin", "selasa", "rabu");
+// versi baru
+$bulan = ["januari", "februari", "maret"];
+// tipe data yg berbeda
+$arr = [123, "string", true];
+
+// menampilkan array versi debugging
+var_dump($hari);
+echo "<br>";
+print_r($bulan);
+echo "<br>";
+
+// menampilkan 1 elemen pada array
+echo $arr[0];
+echo "<br>";
+
+?>
