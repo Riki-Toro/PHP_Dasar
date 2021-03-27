cadd8702d16cbc9dcd9adcfb076d2d0bdd83abfd
Riki Widiantoro <77282337+Riki-Toro@users.noreply.github.com>
2021-03-27 14:03:54 +0700

    Create indexTugas.php

---
+++ pertemuan8-php&mysql/tugas/indexTugas.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Toko Riki</title>
+</head>
+<body>
+    <h1>Daftar Barang Toko Riki</h1>
+    <table border="1" cellpadding="10">
+        <tr>
+            <th>No.</th>
+            <th>Gambar</th>
+            <th>Nama Barang</th>
+            <th>Rak</th>
+            <th>Harga</th>
+            <th>Diproduksi</th>
+            <th>Expired</th>
+            <th>Aksi</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td><img src="gambar/beras.jpg" width="50"></td>
+            <td>Beras</td>
+            <td>Rak 1</td>
+            <td>Rp. 50000</td>
+            <td>PT Bulog (Persero)</td>
+            <td>13-05-2021</td>
+            <td>
+                <a href="">Edit</a>
+                <a href="">Hapus</a>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
